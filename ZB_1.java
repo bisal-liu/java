aa5c38a0dcff15a5f4d1be7120a04d1594380326
bisal-liu <bisal_liu@126.com>
2017-09-03 00:24:59 +0800

    Update ZB_1.java

--- ZB_1.java
+++ ZB_1.java
@@ -1,13 +1,53 @@
-／*
-  Author:      bisal
-  Version:     1.0
-  Description: Find blocking session and provide kill sql
-*／
+    package com.bisal.zb;
 
-ACCEPT SQL_TEXT PROMPT 'SQL TEXT: '
+    import java.io.FileNotFoundException;
+    import java.io.FileReader;
+    import java.io.FileWriter;
+    import java.io.IOException;
 
-exec print_table('select sid, event, username, sql.sql_text from v$session s, v$sql sql where s.sql_id=sql.sql_id and sid <> (select distinct sid from v$mystat) and sql.sql_text like ''%&SQL_TEXT%''');
+    import com.google.gson.JsonArray;
+    import com.google.gson.JsonIOException;
+    import com.google.gson.JsonObject;
+    import com.google.gson.JsonParser;
+    import com.google.gson.JsonSyntaxException;
 
-exec print_table('select sid, blocking_session from v$session where sid=(select sid from v$session s, v$sql sql where s.sql_id=sql.sql_id and sid <> (select distinct sid from v$mystat) and sql.sql_text like ''%&SQL_TEXT%'')');
+    public class ZB_1 {
+        public static void main(String[] args) {
+            try {
+                JsonParser parser = new JsonParser(); // 创建JSON解析器
+                JsonObject object = (JsonObject) parser.parse(new FileReader(
+                        "file/zhiban.log")); // 创建JsonObject对象
+                JsonArray array = object.get("dlist").getAsJsonArray();
 
-select 'alter system kill session '''||sid||','|| serial# ||''';' from v$session where sid=(select sid from v$session where sid=(select blocking_session from v$session s, v$sql sql where s.sql_id=sql.sql_id and sid <> (select distinct sid from v$mystat) and sql.sql_text like '%&SQL_TEXT%'));
+                // 得到为json的数组
+                FileWriter writer = new FileWriter("file/output1.txt");
+                String dName;
+                String dPerson;
+                String dEmail;
+                String dPhone;
+                String dDate;
+                for (int i = 0; i < array.size(); i++) {
+                    JsonObject subObject = array.get(i).getAsJsonObject();
+                    dutyName = subObject.get("dName").getAsString();
+                    dutyPerson = subObject.get("dPerson").getAsString();
+                    dutyPhone = subObject.get("dPhone").getAsString();
+                    dutyEmail = subObject.get("dEmail").getAsString();
+                    dutyDate = subObject.get("startDate").getAsString()
+                            .substring(0, 10);
+                    writer.write("日期=[" + dDate + "] 值班项=[" + dName
+                            + "] 值班人=[" + dPerson + "] 邮箱=[" + dEmail
+                            + "] 电话=[" + dPhone + "]\n");
+                }
+                writer.close();
+            } catch (JsonIOException e) {
+                e.printStackTrace();
+            } catch (JsonSyntaxException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
